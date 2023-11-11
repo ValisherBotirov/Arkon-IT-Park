71cc4332ea23182464b3ddf15f81391c947a5020
firdavs abdullajonov <turgunovsaloxiddin018@gmail.com>
2023-11-11 16:09:13 +0500

    test

--- src/components/card/branCard.vue
+++ src/components/card/branCard.vue
@@ -7,43 +7,41 @@ interface Props {
 
 defineProps<Props>()
 </script>
-
 <template>
-            
     <div>
         <p class="text-3xl text-dark dark:text-white">Гранитные камни</p>
         <div class="flex justify-around mt-5">
-                    <div
-                        class="py-[15px] px-[25px] dark:bg-[#1A1A1A] rounded-[25px] flex items-center justify-center gap-4 xs:gap-[0px] bg-[#FAFAFA]">
-                        <router-link to="/homePage" class="flex-shrink-0 ">
-                            <img src="@/assets/icon/btnLogo1.svg" alt="logo"
-                                class="flex-shrink-0 w-[94px] cursor-pointer hidden dark:flex">
-                            <img src="@/assets/icon/btnLogo1_black.svg" alt="logo"
-                                class="flex-shrink-0 cursor-pointer dark:hidden w-[94px]">
-                        </router-link>
-                    </div>
-                    <div
-                        class="py-[22px] px-[30px] dark:bg-[#1A1A1A] rounded-[25px] flex  items-center justify-center gap-4 xs:gap-[30px] bg-[#FAFAFA]">
-                        <router-link to="/product" class="flex-shrink-0 ">
-                            <img src="@/assets/icon/btnLogo2.svg" alt="logo"
-                                class="flex-shrink-0 cursor-pointer w-[94px] hidden dark:flex">
-                            <img src="@/assets/icon/btnLogo2_black.svg" alt="logo"
-                                class="flex-shrink-0 cursor-pointer w-[94px] dark:hidden">
-                        </router-link>
+            <div
+                class="py-[15px] px-[25px] dark:bg-[#1A1A1A] rounded-[25px] flex items-center justify-center gap-4 xs:gap-[0px] bg-[#FAFAFA]">
+                <router-link to="/homePage" class="flex-shrink-0 ">
+                    <img src="@/assets/icon/btnLogo1.svg" alt="logo"
+                        class="flex-shrink-0 w-[94px] cursor-pointer hidden dark:flex">
+                    <img src="@/assets/icon/btnLogo1_black.svg" alt="logo"
+                        class="flex-shrink-0 cursor-pointer dark:hidden w-[94px]">
+                </router-link>
+            </div>
+            <div
+                class="py-[22px] px-[30px] dark:bg-[#1A1A1A] rounded-[25px] flex  items-center justify-center gap-4 xs:gap-[30px] bg-[#FAFAFA]">
+                <router-link to="/product" class="flex-shrink-0 ">
+                    <img src="@/assets/icon/btnLogo2.svg" alt="logo"
+                        class="flex-shrink-0 cursor-pointer w-[94px] hidden dark:flex">
+                    <img src="@/assets/icon/btnLogo2_black.svg" alt="logo"
+                        class="flex-shrink-0 cursor-pointer w-[94px] dark:hidden">
+                </router-link>
 
-                    </div>
-                </div>
-                <div class="mt-5">
-                    <div
-                        class="py-[22px] px-[25px] dark:bg-[#1A1A1A] rounded-[25px] flex items-center justify-center gap-4 xs:gap-[0px] bg-[#FAFAFA]">
-                        <router-link to="/union" class="flex-shrink-0 ">
-                            <img src="@/assets/icon/Logo GH.png" alt="logo"
-                                class="flex-shrink-0 w-[142px] cursor-pointer hidden dark:flex">
-                            <img src="@/assets/icon/Logo GH (1).png" alt="logo"
-                                class="flex-shrink-0 cursor-pointer w-[142px]  dark:hidden">
-                        </router-link>
-                    </div>
-                </div>
+            </div>
+        </div>
+        <div class=" mt-5">
+            <div
+                class="py-[22px] px-[25px] dark:bg-[#1A1A1A] rounded-[25px] flex items-center justify-center gap-4 xs:gap-[0px] bg-[#FAFAFA]">
+                <router-link to="/union" class="flex-shrink-0 ">
+                    <img src="@/assets/icon/Logo GH.png" alt="logo"
+                        class="flex-shrink-0 w-[142px] cursor-pointer hidden dark:flex">
+                    <img src="@/assets/icon/Logo GH (1).png" alt="logo"
+                        class="flex-shrink-0 cursor-pointer w-[142px]  dark:hidden">
+                </router-link>
+            </div>
+        </div>
     </div>
 </template>
 
