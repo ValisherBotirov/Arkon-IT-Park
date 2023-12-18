ce92b899a1b397f3eedeffaa0af6733750ba9e7e
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-12-19 00:34:11 +0500

    fix:animation card

--- src/components/card/AnimationCard.vue
+++ src/components/card/AnimationCard.vue
@@ -12,14 +12,15 @@ defineProps<Props>();
     <a
       rel="ar"
       :href="link"
-      class="border border-[#448AF7] rounded-[22.5px] flex items-center gap-6 px-4 py-2"
+      class="cursor-pointer"
     >
-      <AnimationCardSVG />
-      <p
-        class="text-[#448AF7] font-semibold text-xl leading-6 tracking-[0.38px]"
-      >
-        View in your space
-      </p>
+      <img src="@/assets/icon/animbtn.png" alt="animation card">
+<!--      <AnimationCardSVG />-->
+<!--      <p-->
+<!--        class="text-[#448AF7] font-semibold text-xl leading-6 tracking-[0.38px]"-->
+<!--      >-->
+<!--        View in your space-->
+<!--      </p>-->
     </a>
   </div>
 </template>
