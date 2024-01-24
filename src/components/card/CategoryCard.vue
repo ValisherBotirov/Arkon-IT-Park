c2fc18de91ddf332ed1160980a01f8fa3a0a4e76
STQT <106919829+STQT@users.noreply.github.com>
2024-01-24 14:08:15 +0500

    Update CategoryCard.vue

--- src/components/card/CategoryCard.vue
+++ src/components/card/CategoryCard.vue
@@ -15,8 +15,8 @@ defineProps<Props>();
     <router-link :to="`${link}`" class="relative w-full category_card">
       <img :src="img" alt="images" class="w-full h-full object-cover" />
       <p
-        class="category_card_text absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20 font-medium text-xl leading-[23.64px]"
-        :style="{ color: color }"
+        class="absolute left-1/2 bottom-0 -translate-x-1/2 -translate-y-1/2 z-20 font-medium text-xl leading-[23.64px]"
+       :style="{color:color}"
       >
         {{ text }}
       </p>
